304c30990cc47103785a40d14b51b44cd62237c8
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:32:34 -0500

    added homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './Components/HomePage'
+import UserPage from './Components/UserPage'
+import UserWeatherPage from './Components/UserWeatherPage'
+import UpdateUser from './Components/UpdateUser'
+
 
 
 class App extends Component {
--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+const Title = styled.div`
+height: 100%vh;
+font-size: 1.5em;
+text-align: center;
+color: white
+`
+
 class HomePage extends Component {
     render() {
         return(
@@ -12,3 +19,5 @@ class HomePage extends Component {
         )
     }
 }
+
+export default HomePage
