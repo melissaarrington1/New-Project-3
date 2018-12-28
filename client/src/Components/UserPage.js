8c25ae8948664ccf8512087bc535a5ff1dfc1d04
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 18:12:20 -0500

    updated user styling

--- client/src/Components/UserPage.js
+++ client/src/Components/UserPage.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import Button from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 class UserPage extends Component {
     render() {
