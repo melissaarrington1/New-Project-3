9d00c12f915dc5d65f5db7a0f29b2513cd39aaae
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:36:39 -0500

    more components added

---
+++ client/src/Components/UserWeatherPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserWeatherPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UserWeatherPage;
