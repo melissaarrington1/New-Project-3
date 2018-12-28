3aa0a77d4e5fb33d6ce3304ca3c2c54ab50cff2b
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:18:26 -0500

    added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const cityController = require('../controllers/cityController')
+
+router.get('/api/user', userController.index)
+router.post('/api/user/', userController.create)
+router.get('/api/user/:userId', userController.show)
+router.patch('/api/user/:userId', userController.update)
+router.delete('/api/user/:userId', userController.delete)
