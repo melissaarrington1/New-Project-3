6f64425b01e4dae3d226cd850927869d24af068f
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:13:11 -0500

    added user model

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require('../models/User')
+
+const userController = {
+    index: (req, res) => {
+        User.find({})
+            .then((users) => {
+                res.send(users)
+            })
+        },
+    create: (req, res) => {
+        User.create(req.body)
+        .then((user) => {
+            res.send(user)
+        })
+    },
+    show: (req, res) => {
+        User.findById(req.params.userId).populate('favoriteCity')
+        .then((user) => {
+            res.send(user)
+        })
+    },
+    update: (req, res) => {
+        User.findByIdAndUpdate(req.params.userId, req.body, {new:true})
+        .then((updatedUser) => {
+            updatedUser.save()
+            res.send(updatedUser)
+        })
+    },
+    delete: (req, res) => {
+        User.findByIdAndDelete(req.params.userId)
+            .then(() => {
+                res.send(200)
+            })
+    }
+}
+
+
+module.exports = userController
