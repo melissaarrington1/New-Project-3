1f8388b3da4daf540881de6dfe3f46e8e0dc063d
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:48:52 -0500

    added a bit of styling

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Button from '@material-ui/core/Button'
 
 const Title = styled.div`
 height: 100%vh;
@@ -14,7 +15,7 @@ class HomePage extends Component {
         return(
             <Title>
                 <h1>Welcome To Your Weather App</h1>
-                <button>Click Here to Enter</button>
+                <Button variant='contained' color='primary'><Link to='/users'>Click Here to Enter</Link></Button>
             </Title>
         )
     }
