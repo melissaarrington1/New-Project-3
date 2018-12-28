b141c46bf31545df7e46b3f34497a890a81f0681
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-12-28 17:13:45 -0500

    updated app.js in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
